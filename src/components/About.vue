eaf517849ef4cb5f7a621c0c95efa1e52c7c4871
komangkysatridarma <komangkysatridarma@smkwikrama.sch.id>
2023-12-20 19:11:39 +0700

    background

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="container mt-20 pt-16">
+  <div class="container mt-20">
     <section
       id="about"
       class="grid grid-flow-col grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 items-center"
@@ -10,12 +10,12 @@
         <img
           src="@/assets/img/Group 1.png"
           alt=""
-          class="w-full h-auto lg:mt-20 md:max-w-xl md:h-full mx-auto object-cover aspect-w-16 aspect-h-9 sm:max-w-xs sm:h-full"
+          class="w-full h-auto lg:mt-20 md:max-w-lg md:h-full mx-auto object-cover aspect-w-16 aspect-h-9 md:mx-0"
         />
       </div>
       <div>
-        <h2 class="mt-5 mb-0 text-4xl font-bold text-black sm:pl-4">About Me</h2>
-        <p class="mt-12 text-black text-lg md:w-[80%] lg:text-2xl sm:pr-10">
+        <h2 class="text-4xl text-black lg:ml-0 font-bold sm:ml-5 sm:mt-16">About Me</h2>
+        <p class="mt-12 text-black text-lg md:w-[80%] lg:text-2xl sm:mr-4">
           Saya adalah murid SMK Wikrama Bogor dengan jurusan IT. Minat saya
           terfokus pada pengembangan web, khususnya di bidang Front-End. Saat
           ini, saya sedang mendalami bahasa pemrograman JavaScript. Saya
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,18 +5,18 @@
   <div class="container mt-32 mb-0 lg:mt-36 sm:mb-10">
     <div class="bg-white`- flex items-center justify-center">
       <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-current md:text-5xl lg:text-6xl text-black">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-current md:text-5xl lg:text-6xl text-black dark:text-black">
           Hello There!
         </h1>
         <h1 class="mb-4 text-2xl font-bold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl">
           I'm Komang Kysa Tri Darma
         </h1>
-        <h1 class="mb-8 text-lg font-extrabold tracking-tight leading-none text-current md:text-xl lg:text-2xl text-black">
+        <h1 class="mb-8 text-lg font-extrabold tracking-tight leading-none text-current md:text-xl lg:text-2xl text-black dark:text-black">
           Back-End Developer
         </h1>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <p class="mr-4 text-lg text-black">Tech Stack:</p>
-          <div class="flex items-center flex-wrap space-x-4 sm:space-x-6 pl-4">
+          <div class="flex items-center flex-wrap space-x-4 sm:space-x-6">
             <img src="@/assets/img/800px-HTML5_Badge.svg.png" alt="HTML" class="w-8 h-8 mb-2 " />
             <img src="@/assets/img/CSS3_logo.svg.png" alt="CSS" class="w-8 h-8 mb-2 " />
             <img src="@/assets/img/JavaScript-logo.png" alt="JavaScript" class="w-8 h-8 mb-2 " />
